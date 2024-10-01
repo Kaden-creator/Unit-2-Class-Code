ec14d8dea6c67a27d93389c794c6d6499358613d
Kaden-creator <meirk@wlhs.wlwv.k12.or.us>
2024-10-01 16:21:17 +0000

    done

--- Practice.py
+++ Practice.py
@@ -12,7 +12,7 @@ ounces_2 = float(input(f"How many ounces of {ingredient_2} do you want?:"))
 ingredient_3 = input("Name a ingredient that you want in your salad:")
 ounces_3 = float(input(f"How many ounces of {ingredient_3} do you want?:"))
 
-num_serving = input("What is the amount of servings you want?:")
+num_serving = int(input("What is the amount of servings you want?: "))
 
 total_ingredient_1 = ounces_1*num_serving
 total_ingredient_2 = ounces_2*num_serving
