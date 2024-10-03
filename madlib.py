d02c1df4d8cf9bd977a2ad9608b77e3b27bb69b4
Kaden-creator <meirk@wlhs.wlwv.k12.or.us>
2024-10-03 16:43:28 +0000

    saving

---
+++ madlib.py
@@ -0,0 +1,31 @@
+"""
+Name: Kaden Meir
+Date: 10/3/24
+Description: Mad Lib project
+"""
+
+person_name = input("What is your name?:")
+players_date = input("What is the date?:")
+
+
+# At least 5 nouns, 5 verbs, 3 adjectives, and 2 adverbs.
+noun_1 = input("I need a noun:")
+noun_2 = input("I need a noun:")
+noun_3 = input("I need a noun:")
+noun_4 = input("I need a noun:")
+noun_5 = input("I need a noun:")
+
+verb_1 = input("I need a verb:")
+verb_2 = input("I need a verb:")
+verb_3 = input("I need a verb:")
+verb_4 = input("I need a verb:")
+verb_5 = input("I need a verb:")
+
+adjective_1 = input("I need a adjective:")
+adjective_2 = input("I need a adjective:")
+adjective_3 = input("I need a adjective:")
+
+adverb_1 = input("I need a adverb:")
+adverb_2 = input("I need a adverb:")
+
+print(f"")
